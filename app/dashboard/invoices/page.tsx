33802cf050c7863dbf55a7d7ee6547cf1b21bab4
Ali <ahsanaliziddi@gmail.com>
2025-08-15 15:55:07 +0500

    final changes are made and app is ready

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,12 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { fetchFilteredInvoices, fetchInvoicesPages } from '@/app/lib/data';
+import {fetchFilteredInvoices, fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices | Acme Dashboard',
+};
  
 export default async function Page(props: {
   searchParams?: Promise<{
